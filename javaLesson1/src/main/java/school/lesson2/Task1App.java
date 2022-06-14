d8813b2e29b55f1aa481fc5d7c1bcad5ca41d7ca
AnnaSik <a.sikorskaya7@gmail.com>
2022-06-14 16:18:44 +0200

    First commit Lesson2

---
+++ javaLesson1/src/main/java/school/lesson2/Task1App.java
@@ -0,0 +1,13 @@
+package school.lesson2;
+
+public class Task1App {
+    public static boolean firstMethod(int a, int b) {
+        int sum = a+b;
+        if(sum>10 && sum<=20){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
